6fcdb890b67820269b1d27e7107fff808cbbee92
steph <thesqrtminus1@gmail.com>
2020-10-28 02:27:24 -0400

    update do.sh
    Former-commit-id: eea5a913f62935bbb01770aec6218304a98df28a

--- do.sh
+++ do.sh
@@ -10,9 +10,10 @@ git reset --hard origin/master;
 git config pull.rebase false;
 git checkout master;
 echo "pulling git"
-git pull;
+git pull -f --progress;
 echo "chmodding";
 chmod 744 build.sh;
 chmod 744 start.sh;
 echo "starting compile script";
 bash ./build.sh;
+
