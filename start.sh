54acb8396f695f99f92d704b0a49eea421615ebe
steph <thesqrtminus1@gmail.com>
2021-04-14 01:44:31 +0000

    Oops.
    Former-commit-id: 93090da268af2512df4663bc498fdcb75b44e644

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 echo `realpath ./`
-steamcmd/steamcmd.sh +login anonymous +force_install_dir `realpath ./` +app_update 232250 +exit
+steamcmd/steamcmd.sh +login anonymous +force_install_dir `realpath ./` +app_update 232250 validate +exit
 ./srcds_run $*
