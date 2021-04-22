fd4ee2d43b1450f7936b1cdae6c201a4f2a9ee4c
steph <thesqrtminus1@gmail.com>
2021-04-22 03:33:26 -0400

    use updated a2s stuff to prevent reflection attacks
    Former-commit-id: 61f553a6dec24dcfa40070ae9ed244b8d48d6d24

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo `realpath ./`
+echo $(realpath ./)
 export STEAM_GAMESERVER_RATE_LIMIT_200MS=25
-steamcmd/steamcmd.sh +login anonymous +force_install_dir `realpath ./` +app_update 232250 +exit
+export STEAM_GAMESERVER_PACKET_HANDLER_NO_IPC=1
+steamcmd/steamcmd.sh +login anonymous +force_install_dir $(realpath ./) +app_update 232250 +exit
+
 ./srcds_run $*
