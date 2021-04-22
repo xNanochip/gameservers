ea8eba706ee3f58feeecb8ac90af67d7847146cc
steph <thesqrtminus1@gmail.com>
2021-04-22 03:19:18 -0400

    update
    Former-commit-id: d307e109ac3cf441dccc388b4583f794d48d57d7

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 echo `realpath ./`
+export STEAM_GAMESERVER_RATE_LIMIT_200MS=25
 steamcmd/steamcmd.sh +login anonymous +force_install_dir `realpath ./` +app_update 232250 +exit
 ./srcds_run $*
