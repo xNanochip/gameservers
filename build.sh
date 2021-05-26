20769c11edd1febe9eef8d167ddb679fcbb4b025
steph <thesqrtminus1@gmail.com>
2021-05-26 05:05:26 +0000

    sigh
    Former-commit-id: a34af8c09b427b11832f8aec09c334f121947730

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ SPCOMP_PATH=$(realpath "tf/addons/sourcemod/scripting/spcomp64")
 COMPILED_DIR=$(realpath 'tf/addons/sourcemod/plugins/')
 SCRIPTS_DIR=$(realpath 'tf/addons/sourcemod/scripting/')
 
-chmod 744 $SPCOMP_PATH
+chmod 744 "$SPCOMP_PATH"
 
-git diff --name-only HEAD $1 | grep "\.sp$" > ./00
+git diff --name-only HEAD "$1" | grep "\.sp$" > ./00
 
 # ==========================
 # Compile all scripts that don't have any smxes
@@ -16,19 +16,19 @@ git diff --name-only HEAD $1 | grep "\.sp$" > ./00
 
 echo "Seeking for .sp in $SCRIPTS_DIR/**/*"
 
-for p in $SCRIPTS_DIR/**/*
+for p in "$SCRIPTS_DIR"/**/*
 do
-    if [ ${p##*.} == 'sp' ]; then
-        if [[ $p =~ "$PLUGIN_NAME/" ]] || [[ $p =~ "include/" ]] || [[ $p =~ "disabled/" ]] || [[ $p =~ "external/" ]] || [[ $p =~ "economy/" ]]; then
+    if [ "${p##*.}" == 'sp' ]; then
+        if [[ $p =~ "stac/" ]] || [[ $p =~ "include/" ]] || [[ $p =~ "disabled/" ]] || [[ $p =~ "external/" ]] || [[ $p =~ "economy/" ]]; then
             continue
         fi
-        PLUGIN_NAME=`realpath --relative-to $SCRIPTS_DIR $p`
+        PLUGIN_NAME=$(realpath --relative-to "$SCRIPTS_DIR" "$p")
         PLUGIN_NAME=${PLUGIN_NAME%.*}
         PLUGIN_SCRIPT_PATH="$SCRIPTS_DIR/$PLUGIN_NAME.sp"
-        PLUGIN_COMPILED_PATH="$COMPILED_DIR/$(basename $PLUGIN_NAME).smx"
+        PLUGIN_COMPILED_PATH="$COMPILED_DIR/$(basename "$PLUGIN_NAME").smx"
 
         if [[ ! -f "$PLUGIN_COMPILED_PATH" ]]; then
-            echo $PLUGIN_SCRIPT_PATH >> ./00
+            echo "$PLUGIN_SCRIPT_PATH" >> ./00
         fi
     fi
 done
@@ -40,26 +40,26 @@ echo "========================="
 
 
 echo "[INFO] Starting processing of plugin files."
-while read p; do
-    PLUGIN_NAME=`realpath --relative-to $SCRIPTS_DIR $p`
+while read -r p; do
+    PLUGIN_NAME=$(realpath --relative-to "$SCRIPTS_DIR" "$p")
     PLUGIN_NAME=${PLUGIN_NAME%.*}
     PLUGIN_SCRIPT_PATH="$SCRIPTS_DIR/$PLUGIN_NAME.sp"
-    PLUGIN_COMPILED_PATH="$COMPILED_DIR/$(basename $PLUGIN_NAME).smx"
+    PLUGIN_COMPILED_PATH="$COMPILED_DIR/$(basename "$PLUGIN_NAME").smx"
 
 
     if [[ ! -f "$PLUGIN_SCRIPT_PATH" ]]; then
         if [[ -f "$PLUGIN_COMPILED_PATH" ]]; then
-            rm $PLUGIN_COMPILED_PATH;
+            rm "$PLUGIN_COMPILED_PATH";
         fi
     fi
 
-    if [[ $p =~ "$PLUGIN_NAME/" ]] || [[ $p =~ "include/" ]] || [[ $p =~ "disabled/" ]] || [[ $p =~ "external/" ]] || [[ $p =~ "economy/" ]] || [[ ! -f "$PLUGIN_SCRIPT_PATH" ]]; then
+    if [[ $p =~ "stac/" ]] || [[ $p =~ "include/" ]] || [[ $p =~ "disabled/" ]] || [[ $p =~ "external/" ]] || [[ $p =~ "economy/" ]] || [[ ! -f "$PLUGIN_SCRIPT_PATH" ]]; then
         continue
     fi
 
-    echo $PLUGIN_SCRIPT_PATH;
+    echo "$PLUGIN_SCRIPT_PATH";
     if [[ -f "$PLUGIN_SCRIPT_PATH" ]]; then
-        $SPCOMP_PATH -D$SCRIPTS_DIR `realpath --relative-to $SCRIPTS_DIR $PLUGIN_SCRIPT_PATH` -o$PLUGIN_COMPILED_PATH -v0
+        $SPCOMP_PATH -D"$SCRIPTS_DIR" "$(realpath --relative-to "$SCRIPTS_DIR" "$PLUGIN_SCRIPT_PATH")" -o"$PLUGIN_COMPILED_PATH" -v0
     fi
 done < ./00
 rm ./00
