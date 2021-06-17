73844019d665a718fb0846117ae6ef74685a3205
steph <thesqrtminus1@gmail.com>
2021-06-17 05:54:16 +0000

    Revert "export the debug var lol"
    This reverts commit 05ce2c526dd57e204d6719123263abef58240339 [formerly 4e00a648c0953e34b440e145842b0d117ef25572]
    
    Former-commit-id: df1844e339b07e0bdb564c4a8289e917372243c0

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,165 +3,141 @@
 # Helper functions
 source scripts/helpers.sh
 
-gitclean=''
-gitshallow=''
-gitgc_aggressive=''
-gitgc=''
+# Variable initialisation
+WORKING_DIR="tf/addons/sourcemod"
+SPCOMP_PATH="scripting/spcomp64"
+SCRIPTS_DIR="scripting"
+COMPILED_DIR="plugins"
+# Exclusion list, use /dir/ for directories and /file_ for file_*.sp
+EXCLUDED="/stac/ /include/ /disabled/ /external/ /economy/ /attributes/ /discord_"
+EXCLUDED="grep -v -e ${EXCLUDED// / -e }"
 
+# Temporary files
+UNCOMPILED_LIST=$(mktemp)
+UPDATED_LIST=$(mktemp)
 
-usage()
-{
-    echo "Usage, assuming you are running this as a ci script, which you should be"
-    echo "  -c removes all plugins and compiles them from scratch and recursively removes all untracked files in the sourcemod folder"
-    echo "  -s culls ('shallowifies') all repositories to only have the last 25 commits, implies -h"
-    echo "  -a runs aggressive git housekeeping on all repositories (THIS WILL TAKE A VERY LONG TIME)"
-    echo "  -h runs normal git housekeeping on all repositories (git gc always gets run with --auto, this will force it to run)"
-    echo "  -v enables debug printing"
+# TODO: I am pretty sure this needs to be single quoted with double quotes around the vars
+trap "rm -f ${UNCOMPILED_LIST} ${UPDATED_LIST}; popd >/dev/null" EXIT
+
+usage() {
+    echo "This script looks for all uncompiled .sp files"
+    echo "and if a reference is given, those that were updated"
+    echo "Then it compiles everything"
+    echo "Usage: ./build.sh <reference>"
+    exit 1
 }
 
-while getopts 'csahv' flag; do
-    case "${flag}" in
-        c) gitclean='true'              ;;
-        s) gitshallow='true'            ;;
-        a) gitgc_aggressive='true'      ;;
-        h) gitgc='true'                 ;;
-        v) export ctf_show_debug='true' ;;
-        ?) usage && exit 1              ;;
-    esac
-done
-
-
-# dirs to check for possible gameserver folders
-TARGET_DIRS=(/srv/daemon-data /var/lib/pterodactyl/volumes)
-# this is clever and infinitely smarter than what it was before, good job
-WORK_DIR=$(du -s "${TARGET_DIRS[@]}" 2> /dev/null | sort -n | tail -n1 | cut -f2)
-# go to our directory with (presumably) gameservers in it or die trying
-debug "pwd: $(pwd)";
-cd "${WORK_DIR}" || { error "can't cd to workdir $dir!!!"; exit 1; }
-
-# kill any git operations that are running and don't fail if we don't find any
-# PROBABLY BAD PRACTICE LOL
-killall -s SIGKILL -q git || true
-# iterate thru directories in our work dir which we just cd'd to
-for dir in ./*/ ; do
-    # we didn't find a git folder
-    if [  ! -d "$dir".git ]; then
-        # shouldn't this be better quoted or does bash handle that? idr
-        warn "$dir has no .git folder!";
-        # go to the next folder (i think?? should be)
-        continue;
+# Just checking the git refernece is valid
+reference_validation()
+{
+    GIT_REF="${1}"
+    if git rev-parse --verify --quiet "${GIT_REF}" > /dev/null; then
+        info "Comparing against ${GIT_REF}"
+    else
+        error "Reference ${GIT_REF} does not exist"
+        exit 2
     fi
-    # we did find a git folder!
-    # print out our cur folder
-    important "Operating on: $dir"
+}
 
-    debug "pwd: $(pwd)";
-    # go to our server dir or die trying
-    cd "$dir" || { error "can't cd to $dir"; continue; }
-    info "finding empty objects"
-    numemptyobjs=$(find .git/objects/ -type f -empty | wc -l)
-    # you do not need the $ apparently
-    # https://github.com/koalaman/shellcheck/wiki/SC2004
-    if (( numemptyobjs > 0 )); then
-        error "FOUND EMPTY GIT OBJECTS, RUNNING GIT FSCK ON THIS REPOSITORY!";
-        # i'll optimize this later
-        find .git/objects/ -type f -empty -exec rm {} +;
-        warn "fetching before git fscking"
-        git fetch -p
-        warn "fscking!!!"
-        git fsck --full
-        continue;
+# Find all changed *.sp files inside ${WORKING_DIR}
+# Write the full list to a file
+# Remove all the *.smx counterparts that exist
+list_updated()
+{
+    UPDATED=$(git diff --name-only HEAD "${GIT_REF}" . | grep "\.sp$" | ${EXCLUDED})
+    # skip compile if there's nothing *to* compile
+    if [[ -z $UPDATED ]]; then
+        ok "No updated files in diff";
+        return 1;
+    fi
+    info "Generating list of updated scripts"
+    while IFS= read -r line; do
+        # git diff reports the full path, we need it relative to ${WORKING_DIR}
+        echo "${line/${WORKING_DIR}\//}" >> "${UPDATED_LIST}"
+        rm -f "${COMPILED_DIR}/$(basename "${line/.sp/.smx}")"
+    done <<< "${UPDATED}"
+    return 0;
+}
+
+# Find all *.sp files inside ${WORKING_DIR}
+# Select those that do not have a *.smx counterpart
+# And write resulting list to a file
+list_uncompiled()
+{
+    # this may need to be quoted
+    UNCOMPILED=$(find "${SCRIPTS_DIR}" -iname "*.sp" | ${EXCLUDED})
+    info "Generating list of uncompiled scripts"
+    # while loop, read from our uncompiled list we just got
+    while IFS= read -r line; do
+        # if file doesnt exist at compiled dir
+        if [[ ! -f "${COMPILED_DIR}/$(basename "${line/.sp/.smx}")" ]]; then
+            # then tack it on to the end of the temp file we made
+            echo "${line}" >> "${UNCOMPILED_LIST}"
+        fi;
+    done <<< "${UNCOMPILED}"
+
+    # skip compile if there's nothing *to* compile
+    if [[  $(wc -l < "$UNCOMPILED_LIST") == 0 ]]; then
+        ok "No uncompiled .sp files";
+        return 1;
     fi
 
-    # no idea lol
-    #CI_LOCAL_REMOTE=$(git remote get-url origin);
-    #CI_LOCAL_REMOTE="${CI_LOCAL_REMOTE##*@}";
-    #CI_LOCAL_REMOTE=$(echo "$CI_LOCAL_REMOTE" | tr : /)
-    #CI_LOCAL_REMOTE=${CI_LOCAL_REMOTE%.git}
-    #
-    ## pretty sure these are cicd vars? dunno
-    #CI_REMOTE_REMOTE="$CI_SERVER_HOST/$CI_PROJECT_PATH.git"
-    #CI_REMOTE_REMOTE=$(echo "$CI_REMOTE_REMOTE" | tr : /)
-    #CI_REMOTE_REMOTE=${CI_REMOTE_REMOTE%.git}
+    return 0;
+}
 
-    # why do we need to check this?
-    #info "Comparing remotes $CI_LOCAL_REMOTE and $CI_REMOTE_REMOTE."
-    #if [ "$CI_LOCAL_REMOTE" == "$CI_REMOTE_REMOTE" ]; then
+# Iterate over a list files and compile all the *.sp files
+# Output will be ${COMPILED_DIR}/plugin_name.smx
+# If an error is found the function dies and report the failing file
+compile()
+{
+    info "Compiling $(wc -l < "${1}") files"
+    while read -r plugin; do
+        info "Compiling ${plugin}"
+        # compiler path  plugin name      output dir      output file replacing sp with smx
+        ./${SPCOMP_PATH} "${plugin}" -o "${COMPILED_DIR}/$(basename "${plugin/.sp/.smx}")" \
+        -v=2 -z=9 -O=2 -\;=+ #-E
+        # verbose, max compressed, max optimized, require semicolons ( #-E treats errors as warnings )
 
-    info "Comparing branches $(git rev-parse --abbrev-ref HEAD) and $CI_COMMIT_REF_NAME."
-    if [ "$(git rev-parse --abbrev-ref HEAD)" == "$CI_COMMIT_REF_NAME" ]; then
-        debug "branches match"
-        debug "cleaning any old git locks..."
-        # don't fail if there are none
-        # and suppress the stderror if its just telling us there are none
-        rm .git/index.lock -v &> >(grep -v "No such") || true
-        debug "setting git config..."
-
-        git config --global user.email "support@creators.tf"
-        git config --global user.name "Creators.TF Production"
-
-        COMMIT_OLD=$(git rev-parse HEAD);
-
-        if [[ "$gitshallow" == "true" ]]; then
-            warn "shallowifying repo on user request"
-            info "clearing stash..."
-            git stash clear;
-            info "expiring reflog..."
-            git reflog expire --expire=all --all;
-            info "deleting tags..."
-            git tag -l | xargs git tag -d;
-            info "setting git gc to automatically run..."
-            gitgc='true'
+        # if something has gone wrong then stop everything and yell about it
+        if [[ $? -ne 0 ]]; then
+            compile_error "${plugin}"
         fi
-        info "fetching..."
-        git fetch origin "$CI_COMMIT_REF_NAME" --depth 25;
+    done < "${1}"
+    return 0;
+}
 
-        info "resetting..."
-        git reset --hard origin/"$CI_COMMIT_REF_NAME";
+# Auxiliary function to catch errors on spcomp64
+compile_error()
+{
+    error "spcomp64 error while compiling ${1}"
+    exit 255
+}
 
-        info "cleaning cfg folder..."
-        git clean -d -f -x tf/cfg/
+###
+# Script begins here ↓
+pushd ${WORKING_DIR} >/dev/null || exit
+[[ ! -x ${SPCOMP_PATH} ]] && chmod u+x ${SPCOMP_PATH}
 
-        info "cleaning maps folder..."
-        git clean -d -f tf/maps
+# Compile all scripts that have been updated
+if [[ -n ${1} ]]; then
+    reference_validation "${1}"
+    info "Looking for all .sp files that have been updated"
+    list_updated
+    # only compile if we found something to compile
+    if [[ $? -eq 0 ]]; then
+        info "Compiling updated plugins"
+        compile "${UPDATED_LIST}"
+    fi
+fi
 
-        if [[ "$gitclean" == "true" ]]; then
-            warn "recursively cleaning sourcemod folder on user request"
-            git clean -d -f -x tf/addons/sourcemod/plugins/
-            git clean -d -f -x tf/addons/sourcemod/plugins/external
-            git clean -d -f -x tf/addons/sourcemod/data/
-            git clean -d -f -x tf/addons/sourcemod/gamedata/
-        fi
+# Compile all scripts that have not been compiled
+info "Looking for all .sp files in ${WORKING_DIR}/${SCRIPTS_DIR}"
+list_uncompiled
+# only compile if we found something to compile
+if [[ $? -eq 0 ]]; then
+    info "Compiling uncompiled plugins"
+    compile "${UNCOMPILED_LIST}"
+fi
 
-        debug "chmodding"
-        # start script for servers is always at the root of our server dir
-        chmod 744 ./start.sh;
-        # everything else not so much
-        chmod 744 ./scripts/build.sh;
-        chmod 744 ./scripts/str0.py;
-        chmod 744 ./scripts/str0.ini;
-
-        debug "running str0 to scrub steamclient spam"
-        # ignore the output if it already scrubbed it
-        python3 ./scripts/str0.py ./bin/steamclient.so -c ./scripts/str0.ini | grep -v "Failed to locate string"
-
-        # don't run this often
-        info "garbage collecting"
-        if [[ "$gitgc_aggressive" == "true" ]]; then
-            warn "running aggressive git gc!!!"
-            git gc --aggressive --prune=all;
-        elif [[ "$gitgc" == "true" ]]; then
-            warn "running git gc on user request"
-            git gc;
-        else
-            debug "auto running git gc"
-            git gc --auto;
-        fi
-        ok "git repo updated on this server ($dir)"
-        info "running build script"
-
-        ./scripts/build.sh "$COMMIT_OLD";
-
-    fi;
-    cd ../;
-done
+ok "All plugins compiled successfully !"
+exit 0
