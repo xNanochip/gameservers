db99a20e488659f5c06844b1487fdabb4fbad4fc
steph <thesqrtminus1@gmail.com>
2021-06-17 07:05:59 +0000

    Update helpers.sh
    Former-commit-id: 327ab6993669a95a2e96d6022b3b5e0202c9c8b5

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # check if we are in a terminal, if not, set our term var to screen so tput doesn't whine
-set | grep TERM &> /dev/null || { warn "no terminal set, defaulting to screen"; export TERM=screen; }
-
+#set | grep TERM &> /dev/null || { warn "no terminal set, defaulting to screen"; export TERM=screen; }
+export TERM=screen;
 # Colours
 BLACK=$(tput setaf 0)
 RED=$(tput setaf 1)
