808fe1cccd6cb1dbf44c2a76a4b310d0ae66d83d
steph <thesqrtminus1@gmail.com>
2021-06-22 14:59:24 -0400

    stop validating
    Former-commit-id: b4d808c3185353d86094e0b1dc725280147d8b75

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 echo $(realpath ./)
 export STEAM_GAMESERVER_RATE_LIMIT_200MS=25
 export STEAM_GAMESERVER_PACKET_HANDLER_NO_IPC=1
-steamcmd/steamcmd.sh +login anonymous +force_install_dir $(realpath ./) +app_update 232250 validate +exit
+steamcmd/steamcmd.sh +login anonymous +force_install_dir $(realpath ./) +app_update 232250 +exit
 
 ./srcds_run $*
 
