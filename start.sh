b36e58d4fdb7b84e89394865b3aac7679852f7d6
stephanie l <sappho@sappho.io>
2021-07-28 04:46:36 +0000

    Remove fixboot cruft and move it to a pre-startup python script
    Former-commit-id: e006499e9c0df42a13a3f50020a04668d5a46fdf

--- start.sh
+++ start.sh
@@ -1,10 +1,36 @@
 #!/usr/bin/env bash
 
-echo ${PWD}
+# colors
+source .scripts/helpers.sh
+
+# a2s shennanigans
 export STEAM_GAMESERVER_RATE_LIMIT_200MS=25
 export STEAM_GAMESERVER_PACKET_HANDLER_NO_IPC=1
+
+# update server if it needs it
 ./steamcmd/steamcmd.sh +login anonymous +force_install_dir ${PWD} +app_update 232250 +exit
 
-echo "ce_server_index ${SERVER_ID}" > ./tf/cfg/quickplay/_id.cfg
-echo "ctf_regen_info" >> ./tf/cfg/quickplay/_id.cfg
-./srcds_run $*
+# generate our server config
+python3 ./gencfg.py
+
+# wait a second
+sleep 1
+
+# here's the args from our python script, for picking the map
+export py_args="$(cat ./py_args)"
+
+# spew em
+important "PY ARGS = ${py_args}"
+
+# delete that temp file
+rm ./py_args
+
+# print out our full cmd
+echo "./srcds_run $* ${py_args}"
+
+# we're good!
+ok "Starting server..."
+
+
+./srcds_run $* ${py_args}
+
