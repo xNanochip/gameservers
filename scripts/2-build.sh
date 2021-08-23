22236e6ed8a2d1d40df7ee6f82401404d8b6aac1
stephanie l <sappho@sappho.io>
2021-08-23 01:19:06 +0000

    sync files and sleep 0.25 seconds between every plugin compiled so we don't go too fast and reuse memory from our previous compile (is my best guess as to why plugins are corrupting)

--- scripts/2-build.sh
+++ scripts/2-build.sh
@@ -107,9 +107,12 @@ compile()
         info "Compiling ${plugin}"
         # compiler path  plugin name    output dir       output file replacing sp with smx
         ./${SPCOMP_PATH} "${plugin}" -o "${COMPILED_DIR}/$(basename "${plugin/.sp/.smx}")" \
-            -v=2 -z=9 -O=2 -\;=+ -E
-        # verbose, max compressed, max optimized, require semicolons, treat errors as warnings
-
+            -v=2 -\;=+ -E
+        # verbose, require semicolons, treat errors as warnings
+        # sync this file
+        sync "${COMPILED_DIR}/$(basename "${plugin/.sp/.smx}")"
+        # wait just a second
+        sleep 0.25
         # if something has gone wrong then stop everything and yell about it
         if [[ $? -ne 0 ]]; then
             error "spcomp error while compiling ${plugin}"
