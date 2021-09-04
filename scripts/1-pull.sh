9d80a5a4b52d8d92aed56ce9a135a8564e46b01c
stephanie l <sappho@sappho.io>
2021-09-04 02:54:05 +0000

    don't ask questions you're not prepared to handle the answers to

--- scripts/1-pull.sh
+++ scripts/1-pull.sh
@@ -102,8 +102,12 @@ git checkout --detach HEAD -f
 info "-> deleting our old branch"
 git branch -D ${ourbranch}
 
-info "-> pulling our new branch from origin"
-git pull origin ${ourbranch}:${ourbranch} --force
+# don't ask questions you're not prepared to handle the answers to
+info "-> getting our branch from origin with ref bullshit
+git fetch origin refs/heads/${ourbranch}:refs/remotes/origin/${ourbranch}
+
+# info "-> pulling our new branch from origin"
+# git pull origin ${ourbranch}:${ourbranch} --force
 
 info "-> checking out ${ourbranch}"
 git checkout -B ${ourbranch} origin/${ourbranch}
